91d7dea8ae982a8f4eeef28807d4e95bde96108a
Rdunc7 <robld2002@gmail.com>
2025-11-21 23:07:56 -0500

    add commands to entrypoint.sh

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -4,5 +4,7 @@
 echo "Making migrations and migrating the database. "
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --run-syncdb --noinput
 python3 manage.py collectstatic --noinput
 exec "$@"
